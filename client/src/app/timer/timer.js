8c302cefab9a7d95acaa65378f50007b2eca1674
Kevin Mann <kevinrmann@gmail.com>
2014-08-27 08:40:02 -0600

    Building out the timer directive functionality

--- client/src/app/timer/timer.js
+++ client/src/app/timer/timer.js
@@ -16,14 +16,19 @@ angular.module('rm.timer', [
 }])
 
 
-.controller('CreateTimerCtrl', [function () {
-  var defaultTimer = {
-
+.controller('CreateTimerCtrl', ['$scope', function ($scope) {
+  $scope.data = {
+    name: "Untitled",
+    type: "up",
+    status: "off",
+    description: "",
+    currentTime: 0,
+    created: Date.now()
   };
 }])
 
 
-.directive('timer', function() {
+.directive('timer', ['$interval', function($interval) {
   return {
     templateUrl: 'timer/timer.tpl.html',
     replace: true,
@@ -32,7 +37,30 @@ angular.module('rm.timer', [
       timerData: '='
     },
     link: function ($scope, iElement, iAttrs) {
-
+      $scope.toggleType = function() {
+        if ($scope.timerData.type === 'up') {
+          $scope.timerData.type = 'down';
+          $scope.timeNotSet = true;
+        } else {
+          $scope.timerData.type = 'up';
+          $scope.timeNotSet = false;
+        }
+      };
+      $scope.resetTimer = function() {
+        $scope.timerData.currentTime = 0;
+        $scope.timerData.created = Date.now();
+        $scope.timerData.status = 'off';
+      };
+      $scope.togglePlayPause = function() {
+        if ($scope.timerData.status === 'on') {
+          $scope.timerData.status = 'off';
+        } else {
+          $scope.timerData.status = 'on';
+        }
+      };
+      $scope.setCountdown = function() {
+        $scope.timeNotSet = false;
+      };
     }
   };
-});
+}]);
